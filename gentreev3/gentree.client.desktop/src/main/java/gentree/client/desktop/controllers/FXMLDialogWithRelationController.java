1f6e8e618b7b3b212854d9d2f35c2086b4324b9d
Vanilka <szymkowiak.martyna@gmail.com>
2018-01-10 23:21:00 +0100

    Push state of 10/01/2018

---
+++ gentreev3/gentree.client.desktop/src/main/java/gentree/client/desktop/controllers/FXMLDialogWithRelationController.java
@@ -0,0 +1,11 @@
+package gentree.client.desktop.controllers;
+
+import gentree.client.desktop.domain.Relation;
+
+/**
+ * Created by Martyna SZYMKOWIAK on 21/07/2017.
+ */
+public interface FXMLDialogWithRelationController extends FXMLDialogController {
+
+    void setRelation(Relation relation);
+}
